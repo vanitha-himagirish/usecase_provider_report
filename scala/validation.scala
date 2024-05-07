209eb86598c233cd19375f47765d69e01a400eb2
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-05-07 15:23:34 -0500

    Update validation.scala

--- scala/validation.scala
+++ scala/validation.scala
@@ -3,7 +3,7 @@ val dfcase1 = spark.read.option("multiline","true")
       .json("/FileStore/citi_usecase_provider_report/total_mem_months/")
 dfcase1.where("member_id=57").show()
 
-//Validation of case 1
+//Validation of case 2
 val dfcase2 = spark.read
       .json("/FileStore/citi_usecase_provider_report/total_months_per_year/")
 dfcase2.select("*").where("member_id=57").show()
