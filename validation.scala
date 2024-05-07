f702a99b0df238170ee85f7bbdbd7adec32d2893
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-05-07 15:12:34 -0500

    Create validation.scala

---
+++ validation.scala
@@ -0,0 +1,9 @@
+//Validation of case 1
+val dfcase1 = spark.read.option("multiline","true")
+      .json("/FileStore/citi_usecase_provider_report/total_mem_months/")
+dfcase1.where("member_id=57").show()
+
+//Validation of case 1
+val dfcase2 = spark.read.option("multiline","true")
+      .json("/FileStore/citi_usecase_provider_report/total_months_per_year/")
+dfcase2.select("*").where("member_id=57").show()
